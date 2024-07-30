7af670503455c88f33b51efb96d7b461ebef20bb
CharlotteLekoro <156782118+CharlotteLekoro@users.noreply.github.com>
2024-07-30 13:58:58 +0200

    produsts fetched from api and unstyled navbar

--- Svelte_Store/src/main.js
+++ Svelte_Store/src/main.js
@@ -6,3 +6,76 @@ const app = new App({
 })
 
 export default app
+function productListing() {
+  return {
+      products: [],
+      async fetchProducts() {
+          const response = await fetch('https://fakestoreapi.com/products');
+          this.products = await response.json();
+      },
+      addToCart(product) {
+          // Implement add to cart logic
+          console.log('Added to cart:', product);
+          this.$dispatch('notify', { message: 'Added to cart!' });
+      },
+      addToWishlist(product) {
+          // Implement add to wishlist logic
+          console.log('Added to wishlist:', product);
+          this.$dispatch('notify', { message: 'Added to wishlist!' });
+      }
+  }
+}
+
+// Product detail page
+function productDetail() {
+  return {
+      product: null,
+      async fetchProduct() {
+          const urlParams = new URLSearchParams(window.location.search);
+          const id = urlParams.get('id');
+          const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+          this.product = await response.json();
+      },
+      addToCart() {
+          // Implement add to cart logic
+          console.log('Added to cart:', this.product);
+          this.$dispatch('notify', { message: 'Added to cart!' });
+      }
+  }
+}
+
+// Shopping cart page
+function cart() {
+  return {
+      items: [],
+      get total() {
+          return this.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+      },
+      increaseQuantity(item) {
+          item.quantity++;
+      },
+      decreaseQuantity(item) {
+          if (item.quantity > 1) {
+              item.quantity--;
+          }
+      },
+      removeItem(item) {
+          this.items = this.items.filter(i => i.id !== item.id);
+      },
+      
+  }
+
+}
+
+// Toast notification
+function toast() {
+  return {
+      visible: false,
+      message: '',
+      show(msg) {
+          this.visible = true;
+          this.message = msg;
+          setTimeout(() => this.visible = false, 3000);
+      }
+  }
+}
